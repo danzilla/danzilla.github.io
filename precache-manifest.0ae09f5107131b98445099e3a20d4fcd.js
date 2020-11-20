3d3735c8cf4b6541e239e5bd094e60bcc36e7ced
Dansutan <dan.alphonza@gmail.com>
2020-11-20 16:51:38 -0500

    Updates

---
+++ precache-manifest.0ae09f5107131b98445099e3a20d4fcd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "566a33f54c936e126bd1a81deca200ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e293fd354c6611afd2f6",
+    "url": "/static/css/2.75016c5b.chunk.css"
+  },
+  {
+    "revision": "e293fd354c6611afd2f6",
+    "url": "/static/js/2.cc4966b6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.cc4966b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fa6cdde1e8ef1ba417a",
+    "url": "/static/js/main.c8dbe40d.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
