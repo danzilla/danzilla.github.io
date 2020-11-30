9da50863a8ce6873f2cacdbf0b659b05040ef4d9
Dansutan <dan.alphonza@gmail.com>
2020-11-29 21:04:44 -0500

    img grid with tilt

--- src/react/components/PictureMyself/index.js
+++ src/react/components/PictureMyself/index.js
@@ -1,27 +1,17 @@
-import React, { Component, useState, useEffect, useRef } from "react";
+import React from "react";
 import { connect } from "react-redux";
-import { Carousel, Button, Row, Col } from 'antd';
-import anime from 'animejs';
+import { Row, Col, Image } from 'antd';
 import Tilt from 'react-tilt';
-import { Image } from 'antd';
 import Masonry from 'react-masonry-css'
 
 // get ALL images
 function importAll(r) {
   return r.keys().map(r);
 }
-const images = importAll(require.context('./meMyself', false, /\.(png|jpe?g|svg)$/));
+const images = importAll(require.context('./meMyself', false, /\.(png|jpe?g|svg|mp4)$/));
 
-// SlideShow
-function LandingPage() {
-  // SlideShow Settings
-  const SliderSettings = {
-    autoplay: true,
-    autoplaySpeed: 1000,
-    fade: true,
-    dotPosition: "left"
-  };
-
+// PictureMeMyself
+function PictureMeMyself() {
   // BrewakPoints for grid
   const breakpointColumnsObj = {
     default: 3,
@@ -29,14 +19,10 @@ function LandingPage() {
     700: 3,
     500: 2
   };
-
-  useEffect(() => {
-    //
-  }, []);
-
+  // display grid
   return (
     <Row className="p-2" type="flex" justify="center" align="middle">
-      <Col style={{ width: "100%", height: "100%"}}>
+      <Col style={{ width: "100%", height: "100%" }}>
 
         <Masonry
           breakpointCols={breakpointColumnsObj}
@@ -44,7 +30,11 @@ function LandingPage() {
           columnClassName="my-masonry-grid_column">
           {/* array of JSX items */}
           {images.map((img, index) => (
-            <Image className="card-2" alt="Danustan Alphonza - 'Photograpy', 'coding', 'hobby' " key={index} src={img} />
+            <Tilt className="Tilt" options={{ max : 25 }} >
+              <div className="Tilt-inner"> 
+                <Image className="card-2" alt="Danustan Alphonza - 'Photograpy', 'coding', 'hobby' " key={index} src={img} />
+              </div>
+            </Tilt>
           ))}
         </Masonry>
 
@@ -53,9 +43,9 @@ function LandingPage() {
   );
 }
 
-// State LandingPage
+// State PictureMeMyself
 const mapStateToProps = state => { return { data: state }; };
-export default connect(mapStateToProps)(LandingPage);
+export default connect(mapStateToProps)(PictureMeMyself);
 
 
 
