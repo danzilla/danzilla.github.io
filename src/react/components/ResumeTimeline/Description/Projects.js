ced88cc731142fef54a4a1a825d2f07a1e616de2
Danustan Alphonza <Dan.alphonza@gmail.com>
2021-03-07 14:37:11 -0500

    Added Projects

--- src/react/components/ResumeTimeline/Description/Projects.js
+++ src/react/components/ResumeTimeline/Description/Projects.js
@@ -7,74 +7,73 @@ const { Meta } = Card;
 class Projects extends Component {
   render() {
     return (
-      <>
-        <Row className="py-1" style={{ height: 'auto' }} type="flex" justify="center" align="middle">
-          <Row> <Title className="font-2" style={{ color: "#08979c" }} level={2} type="danger">Projects</Title> </Row>
-          <Row>
-            <Col className="m-1" style={{position: "relative", overflow: "hidden"}}>
-              <Card style={{ width: 220 }}>
-                <Meta className="my-1" title={<a href="https://github.com/danzilla/blingBlaw">Minimalist budget App</a>} />
-                <div>
-                  <Tag color="magenta">NodeJs</Tag>
-                  <Tag color="red">JavaScript</Tag>
-                  <Tag color="volcano">Express</Tag>
-                  <Tag color="orange">PostgreSQL</Tag>
-                  <Tag color="cyan">GraphQL</Tag>
-                  <Tag color="gold">React</Tag>
-                  <Tag color="lime">Redux + Thunk</Tag>
-                  <Tag color="green">Ant.Design</Tag>
-                  <Tag color="blue">React-Router</Tag>
-                  <Tag color="geekblue">REST and GraphQL</Tag>
-                  <Tag color="purple">Docker</Tag>
-                </div>
-              </Card>
-            </Col>
+      <Row className="py-1" type="flex" justify="center" align="middle">
+        
+        <Title className="font-2" style={{ color: "#08979c" }} level={2} type="danger">Projects</Title>
 
-            <Col className="m-1" style={{position: "relative", overflow: "hidden"}}>
-              <Card style={{ width: 220 }}>
-                <Meta className="my-1" title={<><a href="https://github.com/danzilla/quizzy">Quiz App</a></>} />
-                <div>
-                  <Tag color="magenta">NodeJs</Tag>
-                  <Tag color="orange">MongoDB</Tag>
-                  <Tag color="volcano">Express</Tag>
-                  <Tag color="gold">React</Tag>
-                  <Tag color="blue">React-Router</Tag>
-                  <Tag color="purple">Bootstrap</Tag>
-                  <Tag color="green">Axios</Tag>
-                </div>
-              </Card>
+        <Col>
+          <Row justify="center" type="flex" align="middle">
+            <Col md={7} sm={20} className="card-2 m-1 p-1">
+              <Title level={5} type="danger">
+                <a href="https://github.com/danzilla/blingBlaw">Minimalist budget App</a>
+              </Title>
+              <div>
+                <Tag color="magenta">NodeJs</Tag>
+                <Tag color="red">JavaScript</Tag>
+                <Tag color="volcano">Express</Tag>
+                <Tag color="orange">PostgreSQL</Tag>
+                <Tag color="cyan">GraphQL</Tag>
+                <Tag color="gold">React</Tag>
+                <Tag color="lime">Redux + Thunk</Tag>
+                <Tag color="green">Ant.Design</Tag>
+                <Tag color="blue">React-Router</Tag>
+                <Tag color="geekblue">REST and GraphQL</Tag>
+                <Tag color="purple">Docker</Tag>
+              </div>
             </Col>
-
-            <Col className="m-1" style={{position: "relative", overflow: "hidden"}}>
-              <Card style={{ width: 220 }}>
-                <Meta className="my-1" title={<a href="https://github.com/danzilla/cocktailsRecipes">Cocktails Recipes</a>} />
-                <div>
-                  <Tag color="magenta">NodeJs</Tag>
-                  <Tag color="gold">React</Tag>
-                  <Tag color="lime">Redux + Thunk</Tag>
-                  <Tag color="green">Axios</Tag>
-                  <Tag color="blue">React-Router</Tag>
-                  <Tag color="purple">Bootstrap</Tag>
-                </div>
-              </Card>
+            <Col md={7} sm={20} className="card-2 m-1 p-1">
+              <Title level={5} type="danger">
+                <a href="https://github.com/danzilla/quizzy">Quiz App</a>
+              </Title>
+              <div>
+                <Tag color="magenta">NodeJs</Tag>
+                <Tag color="orange">MongoDB</Tag>
+                <Tag color="volcano">Express</Tag>
+                <Tag color="gold">React</Tag>
+                <Tag color="blue">React-Router</Tag>
+                <Tag color="purple">Bootstrap</Tag>
+                <Tag color="green">Axios</Tag>
+              </div>
             </Col>
-
-            <Col className="m-1" style={{position: "relative", overflow: "hidden"}}>
-              <Card style={{ width: 240 }}>
-                <Meta className="my-1" title={<a href="https://github.com/danzilla/intro-to-cryptographic-primitives">Cryptographic Primitives</a>} />
-                <div>
-                  <Tag color="magenta">Python - Flask</Tag>
-                  <Tag color="gold">cryptography</Tag>
-                  <Tag color="lime">Pydes </Tag>
-                  <Tag color="green">Bootstrap</Tag>
-                </div>
-              </Card>
+            <Col md={7} sm={20} className="card-2 m-1 p-1">
+              <Title level={5} type="danger">
+                <a href="https://github.com/danzilla/cocktailsRecipes">Cocktails Recipes</a>
+              </Title>
+              <div>
+                <Tag color="magenta">NodeJs</Tag>
+                <Tag color="orange">MongoDB</Tag>
+                <Tag color="volcano">Express</Tag>
+                <Tag color="gold">React</Tag>
+                <Tag color="blue">React-Router</Tag>
+                <Tag color="purple">Bootstrap</Tag>
+                <Tag color="green">Axios</Tag>
+              </div>
             </Col>
-
+            <Col md={7} sm={20} className="card-2 m-1 p-1">
+              <Title level={5} type="danger">
+                <a href="https://github.com/danzilla/intro-to-cryptographic-primitives">Cryptographic Primitives</a>
+              </Title>
+              <div>
+                <Tag color="magenta">Python - Flask</Tag>
+                <Tag color="gold">cryptography</Tag>
+                <Tag color="lime">Pydes </Tag>
+                <Tag color="green">Bootstrap</Tag>
+              </div>
+            </Col>
           </Row>
-
-        </Row>
-      </>
+        </Col>
+        
+      </Row>
     );
   }
 }
